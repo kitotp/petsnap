dff944a8f264cba38c29579a2fb7723fc53cc58d
Андрей Слива <cryptomaniaaccton@gmail.com>
2025-07-21 17:30:25 +0200

    user login

--- src/app/slices/userSlice.ts
+++ src/app/slices/userSlice.ts
@@ -1,23 +1,65 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import supabase from "../../supabaseClient";
 
-type initialStateType = {
-    username: string
-} 
 
-const initialState: initialStateType = {
-    username: ''
+
+type UserState = {
+    username: string,
+    status: 'idle' | 'loading' | 'failed',
+    error?: string
 }
+
+const initialState: UserState = {
+    username: '',
+    status: 'idle'
+}
+
+type fetchUserByEmailProps = {
+    email: string,
+    password: string
+}
+
+export const fetchUserByEmail = createAsyncThunk<string, fetchUserByEmailProps>(
+    'user/fetchByEmail',
+    async ({ email, password }) => {
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password
+        })
+        if (error) throw new Error('Error while signing in')
+
+        if (!data.session || !data.session.user.email) {
+            throw new Error('No user mail')
+        }
+
+        return data.session.user.email
+    }
+)
 
 const userSlice = createSlice({
     name: 'user',
     initialState: initialState,
     reducers: {
-        addUser(state, action){
+        addUser(state, action) {
             state.username = action.payload
         }
-    }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchUserByEmail.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchUserByEmail.fulfilled, (state, action: PayloadAction<string>) => {
+                state.status = 'idle',
+                    state.username = action.payload
+            })
+            .addCase(fetchUserByEmail.rejected, (state, action) => {
+                state.status = 'failed',
+                    state.error = action.payload ?? action.error.message
+            })
+    },
 })
 
 export default userSlice.reducer
-export const {addUser} = userSlice.actions
-
+export const { addUser } = userSlice.actions
--- src/features/Login/LoginPage.tsx
+++ src/features/Login/LoginPage.tsx
@@ -1,17 +1,35 @@
+import { useState } from "react"
+import { fetchUserByEmail } from "../../app/slices/userSlice"
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
 
 const LoginPage = () => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const dispatch = useAppDispatch()
+
+    const { status, error } = useAppSelector(state => state.user)
+
+    function handleLogin() {
+        dispatch(fetchUserByEmail({ email: username, password: password }))
+    }
+
+    if (status === 'loading') return <div>Loading...</div>
+    if (error) return <div>Error...</div>
+
     return (
         <div className="flex flex-col items-center justify-center">
             <div className="flex flex-col items-center border justify-center gap-2 border-black w-[400px] h-[500px]">
                 <div className="flex flex-row items-center gap-2">
                     <p>Name:</p>
-                    <input placeholder="name..." className="border border-black p-2"></input>
+                    <input placeholder="name..." value={username} onChange={(e) => setUsername(e.target.value)} className="border border-black p-2"></input>
                 </div>
                 <div className="flex flex-row items-center gap-2">
                     <p>Password:</p>
-                    <input placeholder="password..." className="border border-black p-2"></input>
+                    <input placeholder="password..." value={password} onChange={(e) => setPassword(e.target.value)} className="border border-black p-2"></input>
                 </div>
-                <button className="border border-black p-2">Login</button>
+                <button className="border border-black p-2" onClick={handleLogin}>Login</button>
             </div>
         </div>
     )
